211d52660e82bc63c802e7ccff8397138064f484
Ahmed Memon <ahmedpubgking3388@gmail.com>
2025-12-24 22:39:45 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import { motion } from 'framer-motion';
 import { 
@@ -192,5 +193,6 @@ const AboutSection = () => {
     </section>
   );
 };
+
 
 export default AboutSection;
