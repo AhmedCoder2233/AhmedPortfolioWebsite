1807993f70a1156772ca1aa031b673d6679d5a39
Ahmed Memon <ahmedpubgking3388@gmail.com>
2025-12-24 22:23:20 +0500

    Update page.tsx

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FiExternalLink, FiCode } from 'react-icons/fi';
@@ -212,5 +213,6 @@ const Projects = () => {
     </section>
   );
 };
+
 
 export default Projects;
