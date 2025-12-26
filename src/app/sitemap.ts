51a214d891b4437be328ec01fc9d64699379aaa1
Ahmed Memon <ahmedpubgking3388@gmail.com>
2025-12-26 20:06:11 +0500

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,8 +3,10 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ahmed-portfolio.xyz',
+      url: 'https://www.ahmed-portfolio.xyz/',
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
     },
   ]
 }
